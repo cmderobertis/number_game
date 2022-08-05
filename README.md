e8d16155a894231510a6548b7ad8c8a044a97c58
cmderobertis <cmderobertis@gmail.com>
2022-08-05 13:23:33 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Flask Routes
+
+```zsh
+mkdir *flask_dir_name*
+cd *flask_dir_name*
+```
+
+```zsh
+pipenv install flask
+pipenv shell
+```
+
+- [ ] make [server.py](server.py)
---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, session, request, redirect, url_for
+import random
+
+app = Flask(__name__)
+app.secret_key = 'hello hi what is going on here'
+
+
+@app.route('/')
+def index():
+    if not 'number' in session:
+        session['number'] = random.randint(1, 100)
+    return render_template('index.html')
+
+
+@app.route('/guess', methods=['POST'])
+def guess():
+    num = int(session['number'])
+    session['guess'] = request.form['guess']
+    guess = int(session['guess'])
+    if guess > num:
+        session['response'] = 'too high'
+        return redirect('/')
+    elif guess < num:
+        session['response'] = 'too low'
+        return redirect('/')
+    else:
+        session['response'] = 'correct'
+    return redirect('/')
+
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
